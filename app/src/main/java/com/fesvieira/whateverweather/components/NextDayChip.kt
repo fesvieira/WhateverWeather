fc564b8c2a6c39e18e5b9722788a79d1f9cc7ec1
Felipe Silva Vieira <65823376+fesvieira@users.noreply.github.com>
2023-08-11 15:23:16 -0300

    format date

--- app/src/main/java/com/fesvieira/whateverweather/components/NextDayChip.kt
+++ app/src/main/java/com/fesvieira/whateverweather/components/NextDayChip.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,11 +19,19 @@ import coil.compose.AsyncImage
 import com.fesvieira.whateverweather.helpers.formatTemperature
 import com.fesvieira.whateverweather.models.ForecastDay
 import com.fesvieira.whateverweather.ui.theme.TextFieldBackground
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun NextDayChip(
     forecastDay: ForecastDay
 ) {
+    val firstApiFormat = remember { DateTimeFormatter.ofPattern("yyyy-MM-dd") }
+    val date = remember {
+        LocalDate.parse(forecastDay.date , firstApiFormat)
+    }
+
     Column(
         verticalArrangement = Arrangement.spacedBy(12.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -30,7 +39,7 @@ fun NextDayChip(
             .background(TextFieldBackground, RoundedCornerShape(32.dp))
             .padding(vertical = 16.dp, horizontal = 8.dp)
     ) {
-        Text(text = forecastDay.date.takeLast(5), color = Color.White, fontSize = 20.sp)
+        Text(text = "${date.monthValue}/${date.dayOfMonth}".format(forecastDay.date), color = Color.White, fontSize = 20.sp)
 
         AsyncImage(
             model = "https:${forecastDay.day.condition.icon}",
@@ -39,6 +48,6 @@ fun NextDayChip(
                 .size(48.dp)
         )
 
-        Text(text = "${forecastDay.day.avgtemp_c.formatTemperature}", color = Color.White, fontSize = 20.sp)
+        Text(text = forecastDay.day.avgtemp_c.formatTemperature, color = Color.White, fontSize = 20.sp)
     }
 }
